1621ee3e9691e8c63822b7d4f5f169ce500aefea
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-11-04 08:40:54 -0300

    Organização do codigo.

--- app/src/main/java/com/rodrigo/AppJokenpo/MainActivity.java
+++ app/src/main/java/com/rodrigo/AppJokenpo/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
